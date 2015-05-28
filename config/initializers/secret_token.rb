076c4e992449c1a16ab441e7f7951bd25dd4bbfe
Bill Kirtley <bill@spiffy.com>
2015-05-28 14:11:01 -0400

    rails _3.2.19_ new shoulda_context_demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShouldaContextDemo::Application.config.secret_token = '802536b318aaa4e03a63bda54c62f817117b7c1e0a0fba4977f1d6b812980bf43625d1fde206d2809fbafaa08708094cdde1b6974e8d1a9c96ad690a7da477c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShouldaContextDemo::Application.config.session_store :cookie_store, key: '_shoulda_context_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShouldaContextDemo::Application.config.session_store :active_record_store
